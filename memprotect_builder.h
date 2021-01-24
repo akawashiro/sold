f44ddc76070eac25b177e584f87f26675f445b8b
Akira Kawata <akirakawata1@gmail.com>
2021-01-24 12:04:05 +0000

    Save

--- memprotect_builder.h
+++ memprotect_builder.h
@@ -23,10 +23,10 @@ public:
             LOG(INFO) << "MemprotectBuilder::Emit: " << SOLD_LOG_BITS(offsets[i]) << SOLD_LOG_BITS(sizes[i]);
 
             memcpy(mprotect_code_head, memprotect_body_code, sizeof(memprotect_body_code));
-            int32_t* offset_p = (int32_t*)(mprotect_code_head + memprotect_body_addr_offset);
-            // 7 is the length of `lea 0xabbccdd(%rip), %rdi'
-            int32_t offset_v = (offsets[i] & (~(0x1000 - 1))) -
-                               (static_cast<int32_t>(mprotect_code_offset) + static_cast<int32_t>(mprotect_code_head - mprotect_code) + 7);
+            int64_t* offset_p = (int64_t*)(mprotect_code_head + memprotect_body_addr_offset);
+            // 17 is the length of `mov $0xdeadbeefdeadbeef, %rdi; lea (%rip), %rsi'
+            int64_t offset_v = (offsets[i] & (~(0x1000 - 1))) -
+                               (static_cast<int64_t>(mprotect_code_offset) + static_cast<int64_t>(mprotect_code_head - mprotect_code) + 17);
             LOG(INFO) << "MemprotectBuilder::Emit" << SOLD_LOG_BITS(offset_v) << SOLD_LOG_KEY(offset_v);
             *offset_p = offset_v;
             uint32_t* size_p = (uint32_t*)(mprotect_code_head + memprotect_body_size_offset);
@@ -45,22 +45,25 @@ public:
 
     // call SYS_mprotect syscall
     //
-    // lea 0xabbccdd(%rip), %rdi
+    // mov $0xdeadbeefdeadbeef, %rdi
+    // lea (%rip), %rsi
+    // add %rsi, %rdi
     // mov $0xaabbcc, %rsi (size)
     // mov $0x1, %rdx (0x1 = PROT_READ)
     // mov $10, %eax (10 = SYS_mprotect)
     // syscall
-    static constexpr uint8_t memprotect_body_code[] = {0x48, 0x8d, 0x3d, 0xdd, 0xcc, 0xbb, 0x0a, 0x48, 0xc7, 0xc6, 0xcc, 0xbb, 0xaa, 0x00,
-                                                       0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00, 0xb8, 0x0a, 0x00, 0x00, 0x00, 0x0f, 0x05};
+    static constexpr uint8_t memprotect_body_code[] = {0x48, 0xbf, 0xef, 0xbe, 0xad, 0xde, 0xef, 0xbe, 0xad, 0xde, 0x48, 0x8d, 0x35, 0x00,
+                                                       0x00, 0x00, 0x00, 0x48, 0x01, 0xf7, 0x48, 0xc7, 0xc6, 0xcc, 0xbb, 0xaa, 0x00, 0x48,
+                                                       0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00, 0xb8, 0x0a, 0x00, 0x00, 0x00, 0x0f, 0x05};
     // offset to 0xabbccdd
-    static constexpr int memprotect_body_addr_offset = 3;
+    static constexpr int memprotect_body_addr_offset = 2;
     // offset to 0xaabbcc
-    static constexpr int memprotect_body_size_offset = 10;
+    static constexpr int memprotect_body_size_offset = 23;
 
     // ret
     static constexpr uint8_t memprotect_end_code[] = {0xc3};
 
 private:
-    std::vector<int32_t> offsets;
+    std::vector<int64_t> offsets;
     std::vector<uint32_t> sizes;
 };
