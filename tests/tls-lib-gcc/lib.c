416d21b16841cbe9644250b95cf22de693fa8cfc
Akira Kawata <akirakawata1@gmail.com>
2021-03-13 06:33:59 +0000

    Symbol without relocation information causes crash

--- tests/tls-lib-gcc/lib.c
+++ tests/tls-lib-gcc/lib.c
@@ -1,5 +1,6 @@
 #include "lib.h"
 
+int normal_l;
 int return_tls_i() {
     return thread_local_i;
 }
--- tests/tls-lib-gcc/main.c
+++ tests/tls-lib-gcc/main.c
@@ -3,7 +3,11 @@
 
 int main() {
     printf("i = %d\n", return_tls_i());
-    thread_local_j = 3;
+    thread_local_j = 5;
     printf("j = %d\n", thread_local_j);
+    // normal_k = 7;
+    // printf("k = %d\n", normal_k);
+    normal_l = 9;
+    printf("l = %d\n", normal_l);
     return 0;
 }
