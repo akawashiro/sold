6940943ed97a485fc3ff4988a740c39e1d6ace0f
Akira Kawata <akirakawata1@gmail.com>
2021-03-16 13:40:03 +0000

    Use inheritance

--- tests/std_function/fuga.cc
+++ tests/std_function/fuga.cc
@@ -1,5 +1,11 @@
-void CreateXXX();
+#include "hoge.h"
+
+static XXX* p_xxx = nullptr;
 
 void fuga() {
     CreateXXX();
+    std::function<std::unique_ptr<XXX>(int)> f = GenXXX;
+    std::unique_ptr<XXX> xxx = f(10);
+    p_xxx = xxx.release();
+    printf("%s\n", typeid(*p_xxx).name());
 }
--- tests/std_function/hoge.cc
+++ tests/std_function/hoge.cc
@@ -1,21 +1,21 @@
-#include <functional>
-#include <memory>
+#include "hoge.h"
 
-struct XXX {
-    XXX(int n) : n_(n){};
+struct YYY : public XXX {
+    YYY(int n) : n_(n){};
     int n_;
+    int get() { return 42; };
 };
 
-static std::unique_ptr<XXX> = new XXX(42);
+static std::unique_ptr<XXX> u_xxx(new YYY(42));
 
-static std::unique_ptr<XXX> GenXXX(int n) {
-    // static XXX* p = new XXX(n);
-    return std::unique_ptr<XXX>(&xxx_one);
+std::unique_ptr<XXX> GenXXX(int n) {
+    static XXX* p = new YYY(n);
+    return std::unique_ptr<XXX>(p);
 }
 
 void CreateXXX() {
     std::function<std::unique_ptr<XXX>(int)> f = GenXXX;
     std::unique_ptr<XXX> xxx = f(10);
-    printf("%d\n", xxx.release()->n_);
-    // printf("%d\n", xxx_one.n_);
+    printf("%d\n", xxx.release()->get());
+    printf("%d\n", u_xxx.release()->get());
 }
