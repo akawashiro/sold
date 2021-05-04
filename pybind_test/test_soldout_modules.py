228278ba36548fbe2855e277716f31a181385d4d
Akira Kawata <akirakawata1@gmail.com>
2021-05-04 14:22:16 +0900

    Test object features

--- pybind_test/test_soldout_modules.py
+++ pybind_test/test_soldout_modules.py
@@ -1,3 +1,10 @@
 import myadd
+import myobject
 
 print("myadd.myadd(1, 2) = ", myadd.myadd(1, 2))
+
+p = myobject.Pet('Molly')
+print("p = ", p)
+print("p.getName() = ", p.getName())
+p.setName('Charly')
+print("p.getName() = ", p.getName())
