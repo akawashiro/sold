1addf5db642fc9c7c78932c3b96ff95c244b7c66
Akira Kawata <akirakawata1@gmail.com>
2021-05-04 17:19:37 +0900

    Test object related features

--- pybind_test/add.cc
+++ pybind_test/add.cc
@@ -1,5 +1,8 @@
 #include <pybind11/pybind11.h>
 
+namespace py = pybind11;
+using namespace pybind11::literals;
+
 int add(int i, int j) {
     return i + j;
 }
@@ -7,4 +10,10 @@ int add(int i, int j) {
 PYBIND11_MODULE(myadd, m) {
     m.doc() = "pybind11 example plugin";  // optional module docstring
     m.def("myadd", &add, "A function which adds two numbers");
+    m.def("myadd2", &add, "i"_a, "j"_a);
+    m.def("myadd3", &add, "i"_a = 1, "j"_a = 2);
+
+    m.attr("the_answer") = 42;
+    py::object world = py::cast("World");
+    m.attr("what") = world;
 }
--- pybind_test/test_soldout_modules.py
+++ pybind_test/test_soldout_modules.py
@@ -1,10 +1,46 @@
 import myadd
 import myobject
 
-print("myadd.myadd(1, 2) = ", myadd.myadd(1, 2))
+def test_myadd():
+    assert myadd.myadd(1, 2) == 3
 
-p = myobject.Pet('Molly')
-print("p = ", p)
-print("p.getName() = ", p.getName())
-p.setName('Charly')
-print("p.getName() = ", p.getName())
+def test_myadd_keyword():
+    assert myadd.myadd2(i=1, j=2) == 3
+
+def test_myadd_default():
+    assert myadd.myadd3() == 3
+
+def test_myadd_const():
+    assert myadd.the_answer == 42
+    assert myadd.what == "World"
+
+def test_myobject_pet():
+    p = myobject.Pet('Molly')
+    assert p.getName() == "Molly"
+    p.setName('Charly')
+    assert p.getName() == "Charly"
+
+def test_myobject_dog():
+    p = myobject.Dog('Molly')
+    assert p.getName() == "Molly"
+    assert p.bark() == "woof!"
+
+def test_myobject_pet_store():
+    p = myobject.pet_store()
+    assert type(p).__name__ == "Pet"
+
+def test_myobject_pet_store2():
+    p = myobject.pet_store2()
+    assert type(p).__name__ == "PolymorphicDog"
+    assert p.bark() == "woof!"
+
+def test_myobject_overload():
+    p = myobject.Pet('Molly')
+    p.set(10)
+    p.set("hogehoge")
+    assert p.getAge() == 10
+    assert p.getName() == "hogehoge"
+
+def test_myobject_enum():
+    p = myobject.Pet('Lucy', myobject.Pet.Cat)
+    assert p.type == 1
