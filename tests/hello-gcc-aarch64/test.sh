618ab0b298308afcf9eaff7066ff21fc87b0313d
Akira Kawata <akirakawata1@gmail.com>
2021-05-31 20:45:13 +0900

    Use QEMU user mode emulation

--- tests/hello-gcc-aarch64/test.sh
+++ tests/hello-gcc-aarch64/test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-aarch64-linux-gnu-gcc-9 hello.c -o hello
-aarch64-linux-gnu-gcc-9 hello.c -S -o hello.S
-qemu-system-aarch64 -L /usr/aarch64-linux-gnu ./hello
+aarch64-linux-gnu-gcc hello.c -o hello
+aarch64-linux-gnu-gcc hello.c -S -o hello.S
+qemu-aarch64 -L /usr/aarch64-linux-gnu ./hello
 # ../../build/sold hello -o hello.out --section-headers --check-output 
 # ./hello.out
