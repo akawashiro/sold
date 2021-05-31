a78eeea7ff75721dd3de9193940633b0ba6f1f32
Akira Kawata <akirakawata1@gmail.com>
2021-05-31 12:56:20 +0000

    Support the first AArch64

--- tests/hello-gcc-aarch64/test.sh
+++ tests/hello-gcc-aarch64/test.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-aarch64-linux-gnu-gcc hello.c -o hello
-aarch64-linux-gnu-gcc hello.c -S -o hello.S
-qemu-aarch64 -L /usr/aarch64-linux-gnu ./hello
-# ../../build/sold hello -o hello.out --section-headers --check-output 
-# ./hello.out
+aarch64-linux-gnu-gcc-10 hello.c -o hello
+../../build/sold hello -o hello.out --section-headers --check-output --custom-library-path /usr/aarch64-linux-gnu/lib
+qemu-aarch64 -L /usr/aarch64-linux-gnu ./hello.out
