a55f76507c68a69688c2b8d9dfb8c27d46ea7c64
Akira Kawata <akirakawata1@gmail.com>
2021-06-02 22:56:52 +0000

    Fix typo

--- tests/call_once-g++-aarch64/test.sh
+++ tests/call_once-g++-aarch64/test.sh
@@ -2,7 +2,7 @@
 
 arch64-linux-gnu-g++-10 -fPIC -shared -Wl,-soname,libhoge.so -o libhoge.so hoge.cc
 arch64-linux-gnu-g++-10 -fPIC -shared -Wl,-soname,libfuga.so -o libfuga.so fuga.cc libhoge.so
-LD_LIBRARY_PATH=. arch64-linux-gnu-g++-10 -o main main.cc libfuga.so -pthread
+LD_LIBRARY_PATH=. aarch64-linux-gnu-g++-10 -o main main.cc libfuga.so -pthread
 
 mv libfuga.so libfuga.so.original
 GLOG_log_dir=. LD_LIBRARY_PATH=. ../../build/sold -i libfuga.so.original -o libfuga.so.soldout --section-headers --custom-library-path /usr/aarch64-linux-gnu/lib
