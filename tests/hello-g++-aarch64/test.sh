a5375dda2df049cb593ffece014b2bdee5061272
Akira Kawata <akirakawata1@gmail.com>
2021-06-02 23:27:50 +0000

    Fix tests

--- tests/hello-g++-aarch64/test.sh
+++ tests/hello-g++-aarch64/test.sh
@@ -2,4 +2,4 @@
 
 aarch64-linux-gnu-g++-10 hello.cc -o hello
 ../../build/sold hello -o hello.out --section-headers --check-output --custom-library-path /usr/aarch64-linux-gnu/lib
- qemu-aarch64 -L /usr/aarch64-linux-gnu ./hello.out
+qemu-aarch64 -L /usr/aarch64-linux-gnu ./hello.out
--- tests/tls-multiple-module-g++-aarch64/test.sh
+++ tests/tls-multiple-module-g++-aarch64/test.sh
@@ -7,4 +7,4 @@ aarch64-linux-gnu-g++-10 -o main main.cc -ldl
 
 LD_LIBRARY_PATH=. ../../build/sold -i libfugahoge.so.original -o libfugahoge.so.soldout --section-headers --check-output --custom-library-path /usr/aarch64-linux-gnu/lib
 
-LD_LIBRARY_PATH=. --custom-library-path /usr/aarch64-linux-gnu/lib ./main
+LD_LIBRARY_PATH=. qemu-aarch64 -L /usr/aarch64-linux-gnu ./main
