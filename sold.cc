dbf20f71568be6f06dc13c9c394a01e6adbc8e4b
Akira Kawata <akirakawata1@gmail.com>
2021-07-28 14:20:01 +0000

    Disable mprotect

--- sold.cc
+++ sold.cc
@@ -448,7 +448,9 @@ void Sold::CollectArrays() {
     }
     // TODO(akawashiro) In case of executables, this code causes SEGV. I don't
     // kwow the reason.
-    if (!is_executable_) init_array_.emplace_back(mprotect_offset_);
+    // TODO(akawashiro): Emit relocation entry for mprotect_offset_!!!
+    // TODO kokoyaru!!!!
+    // if (!is_executable_) init_array_.emplace_back(mprotect_offset_);
     for (ELFBinary* bin : link_binaries_) {
         uintptr_t offset = offsets_[bin];
         if (std::any_of(exclude_finis_.cbegin(), exclude_finis_.cend(), [bin](const auto s) { return HasPrefix(bin->soname(), s); }))
